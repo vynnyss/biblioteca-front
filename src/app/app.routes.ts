b1b02dace451ab6fd630d3822afb096a26bf2be9
Diegopkg100 <diegopkg100@gmail.com>
2025-09-27 15:26:03 -0300

    Ajuste dos formulários de cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
 import { Pessoa } from './components/cadastro/pessoa/pessoa';
+import { Funcionario } from './components/cadastro/funcionario/funcionario';
 
 export const routes: Routes = [
     {
@@ -9,10 +10,15 @@ export const routes: Routes = [
     },
     {
         path:"home",
-        component: Home 
+        component: Home
     },
     {
         path:"cadastro/pessoa",
         component: Pessoa
+    },
+    {
+        path: "cadastro/funcionario",
+        component: Funcionario
     }
+   
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { Funcionario } from './components/cadastro/funcionario/funcionario';
+import { Pessoa } from './components/cadastro/pessoa/pessoa';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Funcionario,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
