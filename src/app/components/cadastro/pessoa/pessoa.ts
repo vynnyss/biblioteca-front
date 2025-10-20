8c11ad398d20e2212338690c66fbf5ef4b14fe25
Diegopkg100 <diegopkg100@gmail.com>
2025-10-20 00:13:40 -0300

    Alterações na tela de funcionário

--- src/app/components/cadastro/pessoa/pessoa.ts
+++ src/app/components/cadastro/pessoa/pessoa.ts
@@ -11,18 +11,18 @@ mascaraCPF(event: any): void {
   const input = event.target;
   let valor = input.value;
 
-  // Posição atual do cursor
+  
   const posInicial = input.selectionStart;
 
-  // Remove tudo que não for número
+  
   valor = valor.replace(/\D/g, '');
 
-  // Limita a 11 dígitos
+  
   if (valor.length > 11) {
     valor = valor.slice(0, 11);
   }
 
-  // Aplica a máscara
+ 
   if (valor.length > 9) {
     valor = valor.replace(/(\d{3})(\d{3})(\d{3})(\d{1,2})/, '$1.$2.$3-$4');
   } else if (valor.length > 6) {
@@ -31,7 +31,7 @@ mascaraCPF(event: any): void {
     valor = valor.replace(/(\d{3})(\d{1,3})/, '$1.$2');
   }
 
-  // Atualiza o valor e reposiciona o cursor
+  
   input.value = valor;
   input.setSelectionRange(posInicial, posInicial);
 }
